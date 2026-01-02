65c981ea969ed8d958bcae3ae1504b92dd5af79a
rithikaselvaraj15 <rithikaselvaraj22@gmail.com>
2026-01-02 10:43:46 +0530

    Add files via upload

---
+++ Operators_assignment3sum2.java
@@ -0,0 +1,17 @@
+// 2.Write a program to increment a number until it reaches 10 using ++.
+
+
+package com.index;
+
+public class Operators_assignment3sum2 {
+
+	public static void main(String[] args) {
+
+       for(int i=0;i<=10;i++) {
+    	   
+    	   System.out.println(i);
+       }
+
+	}
+
+}
